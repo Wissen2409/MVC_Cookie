2771108b82ecf77e632985d90e1c6452eacdf486
emrahelis <emrahelis@gmail.com>
2024-12-01 11:37:14 +0300

    sjdnjs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using MVC_Cookie.Models;
 
@@ -13,18 +15,39 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+
+    public string CreateSHA512(string inputText)
+    {
+
+        SHA512 sha512 = SHA512.Create();
+        byte[] inputBytes = Encoding.UTF8.GetBytes(inputText);
+        byte[] hashByte = sha512.ComputeHash(inputBytes);
+        StringBuilder builder = new StringBuilder();
+        foreach (byte b in hashByte){
+
+            builder.Append(b.ToString("x2"));
+        }
+        return builder.ToString();
+    }
+
+
     [HttpGet]
     public IActionResult Login()
     {
-
-
         // sayfa ilk açıldığında, eğer cookie varsa, login ekranı açılmadan başka bir view'a yönlendirme işlemi yapalım!!
         if (Request.Cookies["Email"] != null)
         {
             var email = Request.Cookies["Email"];
             var password = Request.Cookies["Password"];
 
-            if(email==LoginUser.UserName && password==LoginUser.Password){
+            // cookie den hashlenmiş verileri çektik!! bizim sabit email ve password değeri ile karşılaştıracağımız için
+            // Email ve password'ude hashlemek gerekmektedir!!
+
+            var shaUserName=CreateSHA512(LoginUser.UserName);
+            var shaPassword = CreateSHA512(LoginUser.Password);
+
+            if (email == shaUserName && password == shaPassword)
+            {
 
                 return View("Welcome");
             }
@@ -45,22 +68,45 @@ public class HomeController : Controller
 
         if (model.Email == LoginUser.UserName && model.Password == LoginUser.Password)
         {
-            var emailOption = new CookieOptions
+
+
+            if (model.RememberMe)
             {
-                Expires = DateTime.Now.AddDays(1),
-                HttpOnly = true,
-                Secure = true,
-                SameSite = SameSiteMode.Strict   // CRFS saldırılarına karşı koruma!!
-            };
-            var passwordOption = new CookieOptions
-            {
-                Expires = DateTime.Now.AddDays(1),
-                HttpOnly = true,
-                Secure = true,
-                SameSite = SameSiteMode.Strict   // CRFS saldırılarına karşı koruma!!
-            };
-            Response.Cookies.Append("Email", model.Email, emailOption);
-            Response.Cookies.Append("Password", model.Password, passwordOption);
+
+                // Cookie, browserin sizi hatırlaması için kullanılan bir yapıdır,!!
+                // Cookie browserin içerisine atılan küçük bir text dosyasıdır!!
+
+                // cookie içerisine birden fazla değer atılabilir!!
+                // Cookie atılırken, bazı ayarlar yapılır tmelede güvenlik ayarları kötü niyetli birilerinin yazılımlar sayesinde cookie'nizi çalmaması için temel güvenlik ayarları yapılabilir
+                // her cookie'nin bir expire tarihi vardır, Expire date verilen cookie expire date de otomatik olarak silinirler!!
+                // cookie atıldıktan sonra, uygulama kapatılsa bile, tekrar tekrar çalıştırdığınızda okunabilir!!
+                // cookie, broewser bazlıdır!! bir browserin attığı bir cookie'i başja bir browser okuyamaz!!
+                // Cookie içerisinde önemli veriler tutulmamalıdır!!(Veri tabanı şifresi vb) Eğer tutacaksanız, kesinlikle cookie içerisine atılan veri şifrelenmelidir!!
+
+
+                var emailOption = new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(1),
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.Strict   // CRFS saldırılarına karşı koruma!!
+                };
+                var passwordOption = new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(1),
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.Strict   // CRFS saldırılarına karşı koruma!!
+                };
+
+                // email ve password verilerini SHA512 ile şifreleyelim 
+                var shaEmail=  CreateSHA512(model.Email);
+                var shaPassword = CreateSHA512(model.Password);
+                Response.Cookies.Append("Email", shaEmail, emailOption);
+                Response.Cookies.Append("Password", shaPassword, passwordOption);
+
+            }
+
             return View("Welcome");
 
         }
